18112e874f0ea20fbaa8fdb1095e2e31095f2432
Sum-sdl <1079569023@qq.com>
2018-10-19 16:26:22 +0800

    add plugin

---
+++ flutter_rent/lib/flutter_plugin.dart
@@ -0,0 +1,29 @@
+import 'dart:async';
+
+import 'package:flutter/services.dart';
+
+class FlutterPlugin {
+  static const String plugin_name = "sum.flutter.utils/device";
+
+  static const String _fun_getPlatformVersion = "getPlatformVersion";
+  static const String _fun_getAppUniqueUUID = "getAppUniqueUUID";
+  static const String _fun_showToast = "showToast";
+
+  static const MethodChannel _channel = const MethodChannel(plugin_name);
+
+  static Future<String> get platformVersion async {
+    final String version = await _channel.invokeMethod(_fun_getPlatformVersion);
+    return version;
+  }
+
+  static Future<String> get getAppUniqueUUID async {
+    return await _channel.invokeMethod(_fun_getAppUniqueUUID);
+  }
+
+  static Future<String> showToast(String msg) async {
+    var map = Map<String, Object>();
+    map["msg"] = msg;
+    map["index"] = 1;
+    return await _channel.invokeMethod(_fun_showToast, map);
+  }
+}
---
+++ flutter_rent/lib/flutter_receiver.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/services.dart';
+
+class FlutterReceiver {
+  static const String plugin_name = "push";
+
+  static const BasicMessageChannel<String> PUSH =
+      const BasicMessageChannel(plugin_name, const StringCodec());
+}
